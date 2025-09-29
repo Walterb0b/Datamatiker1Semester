6b09c4f507e189dddcaf8723d7a1ec278db610bc
Walterb0b <osca0001@stud.ek.dk>
2025-09-29 14:25:46 +0200

    xd

---
+++ src/Lesson14/Comparator/Fish.java
@@ -0,0 +1,25 @@
+package Lesson14.Comparator;
+
+// Fish class with name and weight
+class Fish {
+    private String name;
+    private double weight; // in kg
+
+    public Fish(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + weight + " kg)";
+    }
+}
